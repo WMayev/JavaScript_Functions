f4127a116b4a3aa9134e1a9343f6d7f103e7b5c0
Werka <wm.administracja@proton.me>
2025-03-24 08:11:48 +0100

    Add FizzBuzz exercise solution

--- functions.js
+++ functions.js
@@ -10,9 +10,9 @@ for (let i = '#'; i.length <= 7; i += '#') {
 //FizzBuzz
 
 for (let i = 1; i <= 100; i++) {
-    if ((i % 3) === 0) {
+    if ((i % 3) === 0 && (i % 5) !== 0) {
         console.log('Fizz');
-    } else if ((i % 5) === 0) {
+    } else if ((i % 5) === 0 && (i % 3) !== 0) {
         console.log('Buzz');
     } else if ((i % 3) === 0 && (i % 5) === 0) {
         console.log('FizzBuzz');
