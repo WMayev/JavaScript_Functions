f039ce71f3265655dd87b44524de80447f91ef9f
Werka <wm.administracja@proton.me>
2025-04-01 09:18:57 +0200

    Add user selection with restrictions

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,1 +1,15 @@
 console.log('Hi! Would you like to play a game?');
+
+const getUserChoice = userInput => {
+    userInput = userInput.toLowerCase();
+    // Changes the user input to a small letter.
+
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors'
+        || userInput === 'bomb') {
+            /* Check to make sure that the user typed a valid choice: ‘rock’, ‘paper’, or ‘scissors’.
+            Or cheated answer */
+            return userInput
+        } else {
+            console.log('You have to choose: rock, paper or scissors.');
+        }
+}
