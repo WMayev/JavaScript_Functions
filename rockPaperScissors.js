2bb9eaaeae76c5ac921429dbb2ed4c273e9d2f96
Werka <wm.administracja@proton.me>
2025-04-01 09:28:58 +0200

    Add function to check who win

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -17,7 +17,7 @@ const getUserChoice = userInput => {
 const getComputerChoice = computerNumber => {
     computerNumber = Math.floor(Math.random()* 3);
     // Get number from 0 to 2.
-    
+
     if (computerNumber === 0) {
         return 'rock';
       } else if (computerNumber === 1) {
@@ -26,3 +26,33 @@ const getComputerChoice = computerNumber => {
         return 'scissors';
       }
 }
+
+const determineWinner = (userChoice, computerChoice) => {
+    if (userChoice === computerChoice) {
+        return 'The game was a tie.';
+      }
+      if (userChoice === 'rock') {
+        if (computerChoice === 'paper') {
+          return 'The computer won.';
+        } else {
+          return 'You won!';
+        }
+      }
+      if (userChoice === 'paper') {
+        if (computerChoice === 'scissors') {
+          return 'The computer won.';
+        } else {
+          return 'You won!';
+        }
+      }
+      if (userChoice === 'scissors') {
+        if (computerChoice === 'rock') {
+          return 'The computer won.';
+        } else {
+          return 'You won!';
+        }
+      }
+      if (userChoice === 'bomb') {
+        return "You're cheated, but you won."
+      }
+    };
