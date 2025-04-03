6265cdbd451525fe154b9ac06a9add4ea22b554c
Werka <wm.administracja@proton.me>
2025-04-03 10:10:09 +0200

    Add some rules to the command

--- whileTalk.js
+++ whileTalk.js
@@ -1,5 +1,11 @@
 // While Talk
 
 /* Take a phrase like ‘turpentine and turtles’ 
-and translate it into its “whale talk” equivalent: ‘UUEEIEEAUUEE’. */
+and translate it into its “whale talk” equivalent: ‘UUEEIEEAUUEE’. 
+
+There are a few simple rules for translating text to whale language:
+    1. There are no consonants. Only vowels excluding “y”.
+    2. The u‘s and e‘s are extra long, so we must double them in our program.*/
+
+
 
